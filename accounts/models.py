85b54b1c187966864400b4743622255e74d07064
ahnaimi <naimi.amirhosein.gt@gmail.com>
2022-12-13 12:09:00 -0600

    making custom user model and deploy on postgresql

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, User
+from .Manager import UserManager
 
-# Create your models here.
+
+class UserModel(AbstractBaseUser, PermissionsMixin):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    time_create = models.DateTimeField(auto_now_add=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name']
+
