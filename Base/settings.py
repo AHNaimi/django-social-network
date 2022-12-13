854a434bc374689ab07eaf077035287f041bc84e
ahnaimi <naimi.amirhosein.gt@gmail.com>
2022-12-13 07:28:21 -0600

    connect postgresql to project and makind an app 'home

--- Base/settings.py
+++ Base/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':'github_test_db',
+	'USER':'postgres',
+	'PASSWORD':'postgresql',
+	'HOST':'127.0.0.1',
+	'PORT':'5432',
     }
 }
 
